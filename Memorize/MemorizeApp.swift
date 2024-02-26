82b20906df04956b38e0808a33804c7105fda9a2
Phoenix <pbpham@wisc.edu>
2024-02-25 19:28:30 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Phoenix Pham on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
